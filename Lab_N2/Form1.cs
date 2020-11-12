6af8af0520c70083ae5a2c6e9b0d46c775fb0a43
ADMIN <daniel.mihol@gmail.com>
2020-11-13 01:35:49 +0200

    save menu

--- Lab_N2/Form1.cs
+++ Lab_N2/Form1.cs
@@ -14,21 +14,14 @@ namespace Lab_N2
 {
     public partial class Form1 : Form
     {
-        Button btn;
         Label lbl;
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
 
-            //Icon = new Icon(@"..\..\Properties\test.ico");
-
-            btn = new Button();
-            btn.Text = "Vajuta siia";
-            btn.Location = new Point(50, 10);
-            btn.Height = 50;
-            btn.Width = 150;
-            btn.Click += Btn_Click;
-            Controls.Add(btn);
+            Icon = new Icon(@"..\..\Properties\test.ico");
+            Text = "Paint";
 
 
 
@@ -36,10 +29,10 @@ namespace Lab_N2
             lbl.Text = "Tarkvara";
             lbl.Size = new Size(150, 50);
             lbl.Location = new Point(110, 120);
-            
+
             MainMenu menu = new MainMenu();
             MenuItem menuitem1 = new MenuItem("File");
-            menuitem1.MenuItems.Add("New    Ctrl + N", new EventHandler(menuitem1_About));
+            menuitem1.MenuItems.Add("New    Ctrl + N", new EventHandler(menuitem1_New));
             menuitem1.MenuItems.Add("Open   F3", new EventHandler(menuitem1_Open));
             menuitem1.MenuItems.Add("Save    F2", new EventHandler(menuitem1_Save));
             menuitem1.MenuItems.Add("Exit     alt + F4", new EventHandler(menuitem1_Exit));
@@ -48,7 +41,7 @@ namespace Lab_N2
             menuitem2.MenuItems.Add("Reno   Ctrl + Shift + Z", new EventHandler(menuitem1_Reno));
             menuitem2.MenuItems.Add("Pen", new EventHandler(menuitem1_Pen));
             ToolStripMenuItem menuItem4 = new ToolStripMenuItem("Opera");
-            
+
 
             MenuItem menuitem3 = new MenuItem("Help");
             menuitem3.MenuItems.Add("About F1", new EventHandler(menuitem1_About));
@@ -57,14 +50,40 @@ namespace Lab_N2
             menu.MenuItems.Add(menuitem2);
             menu.MenuItems.Add(menuitem3);
             Menu = menu;
-            
+
+            g = CreateGraphics();
+            Bitmap pic = new Bitmap(750, 500);
+            //picDrawingSurface.Image = pic;
+
+
+
+
+
+        }
+
+        private void menuitem1_New(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            f.ShowDialog();
             
         }
 
+        private void CreateKeyShortcut_KeyDown1(object sender, KeyEventArgs e)
+        {
+            if ((e.Control && e.KeyCode == Keys.Control) || (e.Control && e.KeyCode == Keys.S))
+            {
+                MessageBox.Show(
+                "Выберите один из вариантов",
+                "Сообщение",
+                MessageBoxButtons.YesNo);
+            }
+        }
         private void menuitem1_Pen(object sender, EventArgs e)
         {
-            Icon icon = new Icon(@"..\..\Properties\test.ico");
-            
+            MessageBox.Show(
+            "Выберите один из вариантов",
+            "Сообщение",
+            MessageBoxButtons.YesNo);
         }
 
         private void menuitem1_Reno(object sender, EventArgs e)
@@ -79,7 +98,23 @@ namespace Lab_N2
 
         private void menuitem1_Save(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image |*.jpg|Bitmap image|*.bmp|GIF image|*.gif|PNG" +
+                "image|*.png";
+            SaveDlg.Title = "Save an Image File";
+            SaveDlg.FilterIndex = 4;
+            SaveDlg.ShowDialog();
+            if (SaveDlg.FileName != "")
+            {
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)SaveDlg.OpenFile();
+                switch (SaveDlg.FilterIndex)
+                {
+                   // case 
+                        
+                }
+            
+            }
         }
 
         private void menuitem1_Open(object sender, EventArgs e)
@@ -104,47 +139,6 @@ namespace Lab_N2
 
 
 
-        private void Btn_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-         /*   else if (e.Node.Text == "MessageBox")
-            {
-                MessageBox.Show("MessageBox", "Kõige lihtsam aken");
-                var answer = MessageBox.Show("Tahad inpudBoxi näha?", "Aken koos nupudega", MessageBoxButtons.YesNo);
-                if (answer == DialogResult.Yes)
-                {
-
-                    string text = Interaction.InputBox("Sisesta siia mingi tekst", "InputBox", "Mingi tekst");
-
-                    if (MessageBox.Show("Kas tahad tekst saada Tekskastisse?", "Teksti salvestamine", MessageBoxButtons.OKCancel) == DialogResult.OK)
-                    {
-                        lbl.Text = text;
-                        Controls.Add(lbl);
-                    }
-                    else if (tabControl.SelectedIndex == 2)
-                    {
-                        page1.BackColor = Color.Red;
-                    }
-                }
-
-            }*/
-        
-
-        private void Box_btn_CheckedChanged(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Box_lbl_CheckedChanged(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void RadioButton_Changed(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 
     }
 }
