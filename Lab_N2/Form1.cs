bb2a2952578d47dfa6a6e0ae574f5f86058a4c25
ADMIN <daniel.mihol@gmail.com>
2020-11-14 13:47:34 +0200

    new graphics

--- Lab_N2/Form1.cs
+++ Lab_N2/Form1.cs
@@ -15,11 +15,20 @@ namespace Lab_N2
     public partial class Form1 : Form2
     {
         NotifyIcon ico;
+        Graphics g;
         TrackBar tr1;
         Panel panel1;
+        int x = -1;
+        int y = -1;
+        bool moving = false;
+        Pen pen;
         public Form1()
         {
             InitializeComponent();
+
+            g = panel1.CreateGraphics();
+            pen = new Pen(Color.Black, 5);
+
 
             Icon = new Icon(@"..\..\Properties\test.ico");
             Text = "Paint";
@@ -95,6 +104,33 @@ namespace Lab_N2
 
 
 
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(moving && x!=-1 && y != -1)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+            }
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
         }
 
         private void tr_ValueChanged(object sender, EventArgs e)
